37199729588710c83d56f98833815b9c3184235a
N_G <1460412+guuuuo@users.noreply.github.com>
2022-03-31 17:44:26 +0800

    Initial commit

---
+++ application/src/test/java/com/amoylabs/blog/comment/CommentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amoylabs.blog.comment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
